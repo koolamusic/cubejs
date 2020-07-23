b2b8060c73bcbe5aa99aa72eadf4131fc9640e14
Andrew Miracle <me@andrewmiracle.com>
2020-07-23 14:09:08 +0000

    add other schema

---
+++ schema/Frontier.js
@@ -0,0 +1,61 @@
+// cube(`Evidence_Frontiers`, {
+//   sql: `SELECT * FROM public.frontier`,
+
+//   joins: {
+
+//   },
+
+//   measures: {
+//     count: {
+//       type: `count`,
+//       drillMembers: [country, fullNames, jobTitle]
+//     }
+//   },
+
+//   dimensions: {
+//     country: {
+//       sql: `country`,
+//       type: `string`
+//     },
+
+//     organisation: {
+//       sql: `organisation`,
+//       type: `string`
+//     },
+
+//     typeOfOrganisation: {
+//       sql: `type_of_organisation`,
+//       type: `string`
+//     },
+
+//     fullNames: {
+//       sql: `full_names`,
+//       type: `string`
+//     },
+
+//     jobTitle: {
+//       sql: `job_title`,
+//       type: `string`
+//     },
+
+//     age: {
+//       sql: `age`,
+//       type: `string`
+//     },
+
+//     uic: {
+//       sql: `uic`,
+//       type: `string`
+//     },
+
+//     sex: {
+//       sql: `sex`,
+//       type: `string`
+//     },
+
+//     region: {
+//       sql: `region`,
+//       type: `string`
+//     }
+//   }
+// });
